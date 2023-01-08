92020c78a46688a13c3fc643c5fd7e0d8dd2a38e
Nayara Cristina Jesus <122224446+nayara-cristina-jesus@users.noreply.github.com>
2023-01-08 16:20:45 -0300

    Create HelloWorld.scala

---
+++ HelloWorld.scala
@@ -0,0 +1,12 @@
+/* 
+Autor: Nayara Cristina Jesus
+Date: 08-01-2023
+*/
+
+// Hello World in Scala
+
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello, World!")
+  }
+}
