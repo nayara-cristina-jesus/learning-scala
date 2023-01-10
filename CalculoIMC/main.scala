1a631f4b438d5dac64b45b44bff20b1bc8bff302
Nayara Cristina Jesus <122224446+nayara-cristina-jesus@users.noreply.github.com>
2023-01-09 22:20:42 -0300

    Update main.scala

--- CalculoIMC/main.scala
+++ CalculoIMC/main.scala
@@ -19,13 +19,13 @@ object Main {
   def classifica(resultado:Double): String = {
     if (resultado < 18.5) {
       return "\nMagreza";
-    } else if (resultado < 24.9) {
+    } else if (resultado > 18.5 && resultado < 24.9) {
       return "\nPeso normal";
-    } else if (resultado < 29.9) {
+    } else if (resultado > 25.0 && resultado < 29.9) {
       return "\nSobrepeso";
-    } else if (resultado < 34.9) {
+    } else if (resultado > 30.0 && resultado < 34.9) {
       return "\nObesidade grau I";
-    } else if (resultado < 40.0) {
+    } else if (resultado > 35.0 && resultado < 40.0) {
       return "\nObesidade grau II";
     } else {
       return "\nObesidade grau III";
@@ -35,7 +35,7 @@ object Main {
   def main(args: Array[String]): Unit = {
     // Saida de dados
     println("\nCalculo do IMC")
-    println("\nIMC:" + imc (1.80, 70.0))
+    println("\nIMC:" + Math.round(imc(70.0, 1.80)))
     println(classifica (60.0))
   }
 }
